c86e499eafe6ef34c74ded3fdfb49c65b6c96326
VictorBasso36 <victorbasso36@gmail.com>
2023-03-20 13:51:58 -0300

    Next config update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
          <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
